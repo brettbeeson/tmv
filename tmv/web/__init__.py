5cc1f52fec174680f92b88f359ccb991eec84a1d
brettbeeson <brettbeeson@fastmail.com>
2020-10-29 21:32:38 +1000

    :q

--- tmv/web/__init__.py
+++ tmv/web/__init__.py
@@ -1,6 +1,7 @@
+import logging
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
-from tmv.web.server import Server
+from tmv.server import Server
 from tmv.camera import DFLT_CAMERA_CONFIG_FILE
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
@@ -9,10 +10,14 @@ from tmv.camera import DFLT_CAMERA_CONFIG_FILE
 async_mode = None
 
 # create_app()
-app = Flask(__name__, static_url_path="/")  # , instance_relative_config=True)
+app = Flask(__name__,static_url_path="/")  # default to folder: /static
+
 app.config.from_mapping(
-    SECRET_KEY="adev",
+    SECRET_KEY="moose",
 )
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
+
 socketio = SocketIO(app, host="0.0.0.0")
 server = Server()
 server.config(DFLT_CAMERA_CONFIG_FILE)
@@ -21,12 +26,5 @@ socketio.on_namespace(server)
 
 @app.route('/')
 def index():
-    """ Serve index """
-    #return send_from_directory(resource_filename(__name__, 'resources/'), "index.html")
+    """ Serve index. Others from default /static """
     return send_from_directory("static","index.html")
-    
-#@app.route('/<path:path>')
-#def static_files(path):
-    #""" Serve resources (js, etc) """
-    # return send_from_directory(resource_filename(__name__, 'resources/'), path)
-    #return path
--- tmv/web/static/app.js
+++ tmv/web/static/app.js
@@ -1,9 +1,19 @@
 "use strict";
 
 let ws;
-let rdws;
-let lastImageUrl = "";
+const TMV_SERVER = "http://home.brettbeeson.com.au/"
 
+function camera_switch(e) {
+  let position = e.target.value;
+  ws.emit("switches", { camera: position });
+  ws.emit("req_switches");
+}
+
+function upload_switch(e) {
+  let position = e.target.value;
+  ws.emit("switches", { upload: position });
+  ws.emit("req_switches");
+}
 
 $(document).ready(function () {
   var editor = ace.edit("editor");
@@ -13,6 +23,31 @@ $(document).ready(function () {
   //toastr.options.preventDuplicates = true;
   toastr.options.positionClass = "toast-bottom-center";
 
+  if ("WebSocket" in window) {
+    //connect("ws://localhost:5000");
+    connect();
+  } else {
+    alert("WebSockets are NOT supported by your browser!");
+  }
+
+  // Redirect to http://localhost:80/index.php to pickup lighttpd server with raspap
+  $("#wifi").on("click", function() {
+    let raspap = $("#wifi").attr("href");
+      window.open(raspap,"_blank");
+      // window.open(raspap,"_self"); same window
+  });
+  $("#wifi").attr("href",location.protocol + "//" + location.hostname+ ":80/index.php")
+  
+  $("#server").on("click", function() {
+    // force open in new tab
+    // set via camera_name
+    let server = $("#server").attr("href");    
+      window.open(server,"_blank");
+  });
+  
+  
+
+  
   $("#services").on("click", function () {
     ws.emit("req_services_status");
   });
@@ -22,12 +57,14 @@ $(document).ready(function () {
   $("#files").on("click", function () {
     ws.emit("req_files");
   });
-  $("#error").on("click", function () {
-    ws.emit("raise_error");
-  });
+  $("#error").on("click", () => ws.emit("raise_error"));
   
   $("#restart").on("click", function () {
-    ws.emit("restart");
+    ws.emit("restart_service");
+  });
+  $("#saveandrestart").on("click", function () {
+    ws.emit("camera_config", editor.getValue());
+    ws.emit("restart_service");
   });
 
   $("#camera-switch input").on("click", camera_switch);
@@ -41,50 +78,67 @@ $(document).ready(function () {
     ws.emit("req_camera_config");
   });
 
-  function camera_switch(e) {
-    let position = e.target.value;
-    ws.emit("switches", { camera: position });
-    ws.emit("req_switches");
-  }
+  
+  
+  let cropper
+  let zoom
+  let cropbox
+  
+  $("#zoom").on("click",() => {  
+    $('#zoom-image').remove()
+    $('#zoom-image-div').empty()
+    $('#zoom-image-div').append("<img id='zoom-image'/>")
+    let current_image_src = $('#image').attr("src")
+    $('#zoom-image').attr("src", current_image_src);
+   });
+  $("#modal-zoom").on("shown.bs.modal", function() {
+    // Only get the Cropper.js instance after initialized
+    var $image = $('#zoom-image');
+    
+    $image.cropper({
+      viewMode: 2,
+      aspectRatio: $image.width() / $image.height(),
+      crop: function(event) {
+        cropbox = event.detail // remember last crop
+    }});
+    cropper = $image.data('cropper');
+    $("#close-zoom").on("click",() => {
+      let img = cropper.getImageData()
+      zoom = [(cropbox.x / img.naturalWidth).toFixed(3), (cropbox.y / img.naturalHeight).toFixed(3), (cropbox.width / img.naturalWidth).toFixed(3), (cropbox.height / img.naturalHeight).toFixed(3)]
+      $("#zoom-result").val("zoom = [" + zoom + "]")      
+    });
+  });
 
-  function upload_switch(e) {
-    let position = e.target.value;
-    ws.emit("switches", { upload: position });
-    ws.emit("req_switches");
-  }
-
-  if ("WebSocket" in window) {
-    connect();
-  } else {
-    alert("WebSockets are NOT supported by your browser!");
-  }
-
-  function connect() {
+  function connect(uri) {
     if (ws) {
       if (ws.connected) {
         return;
       }
     }
     var loc = window.location;
-    var new_uri;
-    if (loc.protocol === "https:") {
-      new_uri = "wss:";
+    if (uri == undefined) {
+      // guess it's localhost
+      var new_uri;
+      if (loc.protocol === "https:") {
+        new_uri = "wss:";
+      } else {
+        new_uri = "ws:";
+      }
+      new_uri += "//" + loc.host;
     } else {
-      new_uri = "ws:";
+      new_uri = uri;
     }
-    new_uri += "//" + loc.host;
     toastr.info("Connecting to " + new_uri);
     ws = io(new_uri);
 
     ws.on("connect", function () {
       ws.emit("req_camera_config");
       ws.emit("req_switches");
+      ws.emit("req_camera_name");
     });
 
-    ws.on("camera_config", function (msg) {
-      editor.setValue(msg.toml);
-    });
-
+    ws.on("camera_config", msg =>editor.setValue(msg.toml));
+    
     ws.on("switches", function (msg) {
       // Unfocus current button as we are getting the real value from server
       let ae = document.activeElement;
@@ -98,14 +152,17 @@ $(document).ready(function () {
       }
     });
 
-    ws.on("message", function (msg) {
-      toastr.info(msg);
-    });
+    ws.on("message", msg =>  toastr.info(msg));
+  
+    ws.on("warning", msg => toastr.warning(msg));
 
-    ws.on("warning", function (msg) {
-      toastr.warning(msg);
+    ws.on("camera_name", msg => {
+      $('#camera-name').text(msg);
+      document.title = "TMV - " + msg
+      $('#server').attr('href',TMV_SERVER + msg)
+      
     });
-
+    
     ws.on("files", function (msg) {
       let logta = $("#log-textarea");
       logta.val(logta.val() + "FILES\n");
@@ -136,15 +193,7 @@ $(document).ready(function () {
         logta.scrollTop(logta[0].scrollHeight - logta.height());
       }
     });
-    function _arrayBufferToBase64( buffer ) {
-      var binary = '';
-      var bytes = new Uint8Array( buffer );
-      var len = bytes.byteLength;
-      for (var i = 0; i < len; i++) {
-          binary += String.fromCharCode( bytes[ i ] );
-      }
-      return window.btoa( binary );
-  }
+  
     ws.on("image", function (msg) {
       let imgtag = $("#image");
       let src
@@ -158,28 +207,23 @@ $(document).ready(function () {
         console.log("Error: Bad Image: " + msg);
       }
       imgtag.attr("src", src);
+      toastr.info("Updated image")
     });
 
-    ws.on("close", function () {
-      toastr.warning("Closed connection");
-    });
-
-    ws.on("error", function () {
-      toastr.error("Connection error");
-      // interface blank out?
-    });
+    ws.on("close", () => toastr.warning("Closed connection"));
+    
+    ws.on("error",()  => toastr.error("Connection error"));
+    
   } /** end: on.connect */
-
-  
   setTimeout(statusCheck, 1000);
 });
 
 function statusCheck() {
   if (ws.connected) {
-    $("#connection-status").html("Connected!");
+    $("#connection-status").html("Connected");
     $("#connection-spinner").hide();
   } else {
-    $("#connection-status").html("Connecting...");
+    $("#connection-status").html("Connecting");
     $("#connection-spinner").show();
   }
   setTimeout(statusCheck, 1000);
@@ -220,3 +264,20 @@ function toggle_active(elements, active_element_value) {
     console.log(exc);
   }
 }
+function _arrayBufferToBase64( buffer ) {
+  var binary = '';
+  var bytes = new Uint8Array( buffer );
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+  }
+  return window.btoa( binary );
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).html()).select();
+  document.execCommand("copy");
+  $temp.remove();
+ }
