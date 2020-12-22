9947ac37b371ae78eba31e75993be9da01667056
Brett Beeson <6414228+brettbeeson@users.noreply.github.com>
2020-12-22 18:35:28 +1000

    Update install-tmv.sh

--- scripts/install-tmv.sh
+++ scripts/install-tmv.sh
@@ -2,7 +2,7 @@ echo Installing base dependancies
 sudo apt install -y python3-pip git pijuice-base python3-picamera
 echo Install Pillow dependancies
 sudo apt install -y libjpeg-dev libopenjp2-7 libtiff5
-echo Install psutils dependancies (could remove?)
+echo Install psutils dependancies #(could remove?)
 sudo apt-get install gcc python3-dev
 echo Installing TMV
 sudo python3 setup.py develop   # dev
