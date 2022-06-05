ab72de0dafab8ccea2a90a1699c7dfdf6226b5ea
brettbeeson <brettbeeson@fastmail.com>
2022-06-05 21:58:08 +1000

    Modify to suit aws machine

--- scripts/install-tmv-videod.sh
+++ scripts/install-tmv-videod.sh
@@ -3,8 +3,8 @@ sudo cp scripts/tmv-videod.service /etc/systemd/system/
 systemctl daemon-reload # optional
 sudo mkdir /etc/tmv
 echo Check ownership of /etc/tmv
-sudo chgrp bbeeson /etc/tmv
-sudo chown bbeeson /etc/tmv
+sudo chgrp $USER /etc/tmv
+sudo chown $USER /etc/tmv
 systemctl start tmv-videod
 systemctl enable tmv-videod
 echo Edit /etc/tmv/videod.toml
